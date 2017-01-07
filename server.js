1b933154c09b2dd34c7ff2e2e4d1febc5e91de39
Jason Denning <jdenning33@gmail.com>
2017-01-07 08:50:12 -0700

    Update Server

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ var app = express();
 var router = express.Router();
 
 //set our port to either a predetermined port number if you have set it up, or 3001
-var port = process.env.API_PORT || 3001;
-app.set('port', (port));
+app.set('port', (process.env.API_PORT || 3001));
 
 // Express only serves static assets in production
 if (process.env.NODE_ENV === 'production') {
@@ -19,37 +18,26 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 //db config
-mongoose.Promise = global.Promise;
-mongoose.connect(DB_LINK);
+// mongoose.Promise = global.Promise;
+// mongoose.connect(DB_LINK);
 
 //now we should configure the API to use bodyParser and look for JSON data in the request body
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//To prevent errors from Cross Origin Resource Sharing, we will set our headers to allow CORS with middleware like so:
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
-
-  //and remove cacheing so we get the most recent comments
-  res.setHeader('Cache-Control', 'no-cache');
-  next();
-});
 
 //now  we can set the route path & initialize the API
-router.get('/', function(req, res) {
-  res.json({ message: 'API Initialized!'});
-});
+// router.get('/', function(req, res) {
+//   res.json({ message: 'API Initialized!'});
+// });
 
 
-dataAPIrouter.addToRouter(router);
+// dataAPIrouter.addToRouter(router);
 
 //Use our router configuration when we call /api
 // app.use('/api', router);
 
 //starts the server and listens for requests
-app.listen(port, function() {
-  console.log(`api running on port ${port}`);
+app.listen(app.get('port'), () => {
+  console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
 });
