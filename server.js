fa97c15e431eba80174950747a084b7d0eafd70d
Jason Denning <jdenning33@gmail.com>
2017-01-07 09:25:52 -0700

    try to add router

--- server.js
+++ server.js
@@ -33,7 +33,6 @@ mongoose.connect(uristring, function (err, res) {
   }
 });
 
-//new
 //now we should configure the API to use bodyParser and look for JSON data in the request body
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -48,58 +47,15 @@ app.use(function(req, res, next) {
   res.setHeader('Cache-Control', 'no-cache');
   next();
 });
-//new
 
+var router = express.Router();
+//now  we can set the route path & initialize the API
+router.get('/', function(req, res) {
+  res.json({ message: 'API Initialized!'});
+});
+//Use our router configuration when we call /api
+app.use('/api', router);
 
-// const COLUMNS = [
-//   'carbohydrate_g',
-//   'protein_g',
-//   'fa_sat_g',
-//   'fa_mono_g',
-//   'fa_poly_g',
-//   'kcal',
-//   'description',
-// ];
-// app.get('/api/food', (req, res) => {
-//   const param = req.query.q;
-//
-//   if (!param) {
-//     res.json({
-//       error: 'Missing required parameter `q`',
-//     });
-//     return;
-//   }
-//
-//   // WARNING: Not for production use! The following statement
-//   // is not protected against SQL injections.
-//   const r = db.exec(`
-//     select ${COLUMNS.join(', ')} from entries
-//     where description like '%${param}%'
-//     limit 100
-//   `);
-//
-//   if (r[0]) {
-//     res.json(
-//       r[0].values.map((entry) => {
-//         const e = {};
-//         COLUMNS.forEach((c, idx) => {
-//           // combine fat columns
-//           if (c.match(/^fa_/)) {
-//             e.fat_g = e.fat_g || 0.0;
-//             e.fat_g = (
-//               parseFloat(e.fat_g, 10) + parseFloat(entry[idx], 10)
-//             ).toFixed(2);
-//           } else {
-//             e[c] = entry[idx];
-//           }
-//         });
-//         return e;
-//       })
-//     );
-//   } else {
-//     res.json([]);
-//   }
-// });
 
 app.listen(app.get('port'), () => {
   console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
